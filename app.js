e9fbb3853f3ba2162c9e6b5d2cd8adb15d698af0
frekerzzzz <kannamugesh135@gmail.com>
2024-02-01 13:14:04 +0530

    app commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 const cors = require("cors");
 
 var usersRouter = require("./routes/users");
-var taskRouter = require("./routes/task")
+
 const app = express();
 
 app.use(express.json());
@@ -14,13 +14,6 @@ app.use(
   cors()
 );
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://master--rococo-sundae-8b37d6.netlify.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-// });
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -33,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", usersRouter);
-app.use("/tasks",taskRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
